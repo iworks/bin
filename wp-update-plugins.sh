141f5da2f4067987800a8b77e251b1ba7cb7acdd
Marcin Pietrzak <marcin@iworks.pl>
2026-02-11 11:05:41 +0100

    tune

--- wp-update-plugins.sh
+++ wp-update-plugins.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
-WP_PLUGINS_DIR=/var/virtuals/wordpress/plugins/
+WP_PLUGINS_DIR=/var/virtuals/wordpress/wp-content/plugins
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 cd ${WP_PLUGINS_DIR}
 
 PLUGINS=$(wp plugin status|grep "^ U"|grep -v PLUGIN_VERSION|awk '{print $2}'|grep -v " ")
 
-wp plugin update ${PLUGINS}
-wp language plugin update --all
+if [ -x ${PLUGINS} ]
+then
+    echo -e "${RED}There are no plugins to update.${NC}"
+else
+    wp plugin update ${PLUGINS}
+    wp language plugin update --all
+fi
 
-cd -
+cd - 2>/dev/null 1>&2
+
